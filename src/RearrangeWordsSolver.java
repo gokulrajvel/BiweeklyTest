e1aa485f1e79de5b0783b20c34e561f33c6d2a88
Gokulraj Velu <vgokulraj360@gmail.com>
2026-02-16 16:18:46 +0530

    16-02-2026 Biweekly test

--- src/RearrangeWordsSolver.java
+++ src/RearrangeWordsSolver.java
@@ -8,19 +8,27 @@ public class RearrangeWordsSolver {
         System.out.print(solver.rearrangeWords(str));
     }
     public String rearrangeWords(String str) {
-        String[] words = str.split(" ");
-        int max=Integer.MIN_VALUE;
+        String[] words = str.toLowerCase().split(" ");
+        int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
         for(String word : words){
             max=Math.max(max,word.length());
+            min=Math.min(min,word.length());
         }
         String str1="";
+        boolean flag=true;
         for(int i=0;i<=max;i++){
             for(int j=0;j<words.length;j++){
+                if(flag&&words[j].length()==min){
+                    words[j]=words[j].substring(0,1).toUpperCase() + words[j].substring(1);
+                    flag=false;
+                }
                 if(words[j].length()==i){
                     str1=str1+words[j]+" ";
                 }
+
             }
         }
-        return str1;
+
+        return str1.trim();
     }
 }
